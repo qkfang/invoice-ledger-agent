c9b73731bc5913ddccf8da8e11127e4c654b838c
Daniel Fang <qkfang@hotmail.com>
2026-05-17 11:28:25 +1000

    add

--- bicep/deploy.ps1
+++ bicep/deploy.ps1
@@ -5,3 +5,4 @@ az deployment group create --name 'invledger-dev' --resource-group 'rg-invledger
 $spAppId = az ad sp list --display-name 'sp-demo-01' --query '[0].appId' -o tsv
 $subscriptionId = az account show --query 'id' -o tsv
 az role assignment create --assignee $spAppId --role 'Contributor' --scope "/subscriptions/$subscriptionId/resourceGroups/rg-invledger"
+az role assignment create --assignee $spAppId --role 'User Access Administrator' --scope "/subscriptions/$subscriptionId/resourceGroups/rg-invledger"
