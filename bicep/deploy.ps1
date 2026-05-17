9b30310a03a427394a69f598ec631cf2eb48db26
Daniel Fang <qkfang@hotmail.com>
2026-05-17 10:06:59 +1000

    add role assignment for service principal in deployment script

--- bicep/deploy.ps1
+++ bicep/deploy.ps1
@@ -1,3 +1,7 @@
 az group create --name 'rg-invledger' --location 'australiaeast'
 
 az deployment group create --name 'invledger-dev' --resource-group 'rg-invledger' --template-file './main.bicep' --parameters './main.bicepparam'
+
+$spAppId = az ad sp list --display-name 'sp-demo-01' --query '[0].appId' -o tsv
+$subscriptionId = az account show --query 'id' -o tsv
+az role assignment create --assignee $spAppId --role 'Contributor' --scope "/subscriptions/$subscriptionId/resourceGroups/rg-invledger"
